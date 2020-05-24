6e29438774a418cca11b11bcd4ae976d5c482922
sarori <salo3678@gmail.com>
2020-05-24 23:48:22 +0200

    .

---
+++ exercises/anagrams/index.js
@@ -0,0 +1,25 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+function anagrams(stringA, stringB) {
+	const charMap = {};
+	const strA = stringA.replace(/[^\w]/g, "").toLowerCase();
+	const strB = stringB.replace(/[^\w]/g, "").toLowerCase();
+	for (let char of strA) {
+		if (charMap[char]) {
+			charMap[char]++;
+		} else {
+			charMap[char] = 1;
+		}
+	}
+	console.log(charMap);
+}
+
+anagrams("hi theRe!", "bye There!!!");

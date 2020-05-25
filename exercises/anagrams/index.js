b904adb79c0b673e62cf25f23c6bdacf90746241
sarori <salo3678@gmail.com>
2020-05-25 19:48:15 +0200

    .

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,18 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+//solution1.
+
 function anagrams(stringA, stringB) {
-	const charMap = {};
-	const strA = stringA.replace(/[^\w]/g, "").toLowerCase();
-	const strB = stringB.replace(/[^\w]/g, "").toLowerCase();
-	for (let char of strA) {
-		if (charMap[char]) {
-			charMap[char]++;
-		} else {
-			charMap[char] = 1;
+	const aCharMap = buildCharMap(stringA);
+	const bCharMap = buildCharMap(stringB);
+
+	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+		return false;
+	}
+	for (let char in aCharMap) {
+		if (aCharMap[char] !== bCharMap[char]) {
+			return false;
 		}
 	}
-	console.log(charMap);
+	return true;
 }
 
-anagrams("hi theRe!", "bye There!!!");
+function buildCharMap(str) {
+	const charMap = {};
+
+	for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+		charMap[char] = charMap[char] + 1 || 1;
+	}
+	return charMap;
+}
+
+//solution2
+function anagrams(stringA, stringB) {
+	return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+	return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
